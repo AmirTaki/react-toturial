c3805bee6034475b9a1c8d163fd8fd3c5ffdfa8e
amir <amirtaki623@gmail.com>
2025-05-17 15:48:52 +0330

    syntax jsx part 2

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,32 +1,47 @@
 import { Component } from "react";
 
 class Product extends Component {
+    count = 15;  // constructor attribute
+    name = true;  // attribute
+    number = 10
     render() {
-        const count = 5;
-        const list = [<li>item one</li>,<li>item two</li>,<li>item three</li>]
+        // const count = 5
         const Ltext = ['textOne', "textTwo", "textThree"]
-        // const maplist = Ltext.map(item => <li>{item}</li>)
         return (
             <>
-              <span className='m-2 text-info' >{this.format()}</span>
-              <span className='m-2 badge bg-primary'>{1 - 1}</span>
-              <button className='m-2 btn btn-sm btn-success'>+</button>
-              <button className='m-2 btn btn-sm btn-warning '>-</button>
-              <button className='m-2 btn btn-sm btn-danger'>delete</button>
-
-              <ul>
-                {list}
-              </ul>
-              
-              <ul>     
-                {Ltext.map((item, index) => <li key = {index}>{item}</li>)}
-              </ul>
+             { this.count !== 0 ? (
+                <> 
+                    {/* condition ? true : false */}
+                    <span className='m-2 text-info' >{this.name  ? "product name" :'not' }</span>
+                    {/* function () -> attribute */}
+                    <span className='m-2 badge bg-primary'>{this.format ()}</span>
+                    {/* condition && */}
+                    <span className='m-2 badge bg-primary'>{this.number !== 0 && this.number}</span>
+             
+                    <span className='m-2 badge bg-primary'></span>
+             
+                    <button className='m-2 btn btn-sm btn-success'>+</button>
+             
+                    <button className='m-2 btn btn-sm btn-warning '>-</button>
+             
+                    <button className='m-2 btn btn-sm btn-danger'>delete</button>    
+             
+                    <ul>     
+                        {Ltext.map((item, index) => <li key = {index}>{item}</li>)}
+                    </ul>
+                </>
+          
+             ) : (
+                <h1>there is no product</h1>
+             )}
+        
             </>
         )
     }
-
-    format(){
-        return <a href = "#">product name</a>;
+    format (){
+        // return this.count == 0 ? 'zero' : this.count;
+        if (this.count == 0) return 'zero'
+        else return this.count
     }
 }
 

8e94b57b50770db53079c32569e916a72d3fc865
amir <amirtaki623@gmail.com>
2025-05-17 17:11:37 +0330

    state & setState

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -2,42 +2,67 @@ import { Component } from "react";
 import styled from './product.moudle.css';
 
 class Product extends Component {
-    count = 15;  // constructor attribute
+     // constructor attribute
+
+    // state atribute  => state = {object}
+    state = {
+        count : 0,
+        // productName : 'labtop'
+    }
     render() {
    
 
         return (
             <>  
-                <span className='m-2 text-info' >product name</span>
+                <span className='m-2 text-info' >labtop</span>
             
-                <span className='m-2 badge bg-primary'>{this.format ()}</span>
+                <span className='m-2 badge bg-primary'>{this.format()}</span>
             
                 <span className='m-2 badge bg-primary'></span>
               
-                {/* STAND A LINE => .bind(this) */}
-                <button onClick = {this.handelIncrement.bind(this)} className='m-2 btn btn-sm btn-success'>+</button>
+                <button onClick = {this.handelIncrement} className='m-2 btn btn-sm btn-success'>+</button>
               
-                {/* Arrow Function */}
                 <button onClick = {this.handelDecrement} className='m-2 btn btn-sm btn-warning '>-</button>
               
-                {/*  method (paramter) => {()=> this.method(paramter)} */}
-                <button onClick = {()=> {this.handelDelete(55)}} className='m-2 btn btn-sm btn-danger'>delete</button>    
+                <button onClick = {this.handelDelete} className='m-2 btn btn-sm btn-danger'>delete</button>    
             
            </>
         )
     }
-    handelIncrement(){
-        console.log('Increament', this)
+    handelIncrement = ()=>{
+        // this.state.count += 1; 
+       
+        
+        /* 
+        method setState : 
+            1)
+            this.setState({count : this.state.count + 1})
+            
+            2) 
+            const counter  = this.state.count
+            this.setState({count : counter + 1})
+            
+            3)
+            const {count} = this.state;
+            this.setState({count + 1})
+        */
+    
+        const {count : c} = this.state;
+        this.setState({count : c + 1})
+     
+        console.log(this.state)
     }
+
     handelDecrement = () =>{
-        console.log('Decrement', this)
+        const {count} = this.state;
+        this.setState({count : count - 1})
     }
-    handelDelete(itemNmber){
-        console.log('Delete', itemNmber)
+    handelDelete = () =>{
+        console.log('Delete')
     }
 
     format (){
-        return this.count === 0 ? 'zero' : this.count;
+        return this.state.count === 0 ? 'zero' : this.state.count;
     }
 }
 

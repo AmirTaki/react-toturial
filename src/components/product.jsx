18d1a73c2c74dea42ab94908c99de5b481face98
amir <amirtaki623@gmail.com>
2025-05-17 16:08:24 +0330

    syntax jsx part 3

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,47 +1,33 @@
 import { Component } from "react";
+import styled from './product.moudle.css';
 
 class Product extends Component {
     count = 15;  // constructor attribute
-    name = true;  // attribute
-    number = 10
+    imgUrl = 'https://picsum.photos/200'
     render() {
-        // const count = 5
-        const Ltext = ['textOne', "textTwo", "textThree"]
+   
+
         return (
-            <>
-             { this.count !== 0 ? (
-                <> 
-                    {/* condition ? true : false */}
-                    <span className='m-2 text-info' >{this.name  ? "product name" :'not' }</span>
-                    {/* function () -> attribute */}
-                    <span className='m-2 badge bg-primary'>{this.format ()}</span>
-                    {/* condition && */}
-                    <span className='m-2 badge bg-primary'>{this.number !== 0 && this.number}</span>
-             
-                    <span className='m-2 badge bg-primary'></span>
-             
-                    <button className='m-2 btn btn-sm btn-success'>+</button>
-             
-                    <button className='m-2 btn btn-sm btn-warning '>-</button>
-             
-                    <button className='m-2 btn btn-sm btn-danger'>delete</button>    
-             
-                    <ul>     
-                        {Ltext.map((item, index) => <li key = {index}>{item}</li>)}
-                    </ul>
-                </>
-          
-             ) : (
-                <h1>there is no product</h1>
-             )}
-        
+            <>  
+                <span className='m-2 text-info' >product name</span>
+            
+                <span className='m-2 badge bg-primary'>{this.format ()}</span>
+            
+                <span className='m-2 badge bg-primary'></span>
+            
+                <button className='m-2 btn btn-sm btn-success'>+</button>
+            
+                <button className='m-2 btn btn-sm btn-warning '>-</button>
+            
+                <button className='m-2 btn btn-sm btn-danger'>delete</button>    
+            
+            {/* style :  {}=> javaScript , {}Object */}
+                <img src={this.imgUrl}  style = {{borderRadius: '50%'}} alt="" />
             </>
         )
     }
     format (){
-        // return this.count == 0 ? 'zero' : this.count;
-        if (this.count == 0) return 'zero'
-        else return this.count
+        return this.count === 0 ? 'zero' : this.count;
     }
 }
 
